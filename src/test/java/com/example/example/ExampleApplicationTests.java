78c00f5f6eb419effe273880db60bc37281b6aee
knielse1 <kevinn2055@gmail.com>
2018-06-21 23:08:38 -0500

    updates

--- src/test/java/com/example/example/ExampleApplicationTests.java
+++ src/test/java/com/example/example/ExampleApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExampleApplicationTests {
 
+
 	@Test
-	public void contextLoads() {
+	public void test() {
+		System.out.println("ASDF");
 	}
+
 
 }
